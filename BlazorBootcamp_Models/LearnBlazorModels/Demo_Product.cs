334a3fe9a4a4dedce77fafecdf844f27de459171
Marek Nowakowski <marsznow@gmail.com>
2024-03-10 18:23:30 +0100

    Learning Blazor

---
+++ BlazorBootcamp_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorBootcamp_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
+        public List<Demo_ProductProp> Properties { get; set; }
+    }
+}
